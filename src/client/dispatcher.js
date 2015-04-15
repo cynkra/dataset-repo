2451afee92d75e1930307fb458a6daa41d1564b3
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-04-15 22:49:26 +0200

    Refactored dispatcher (use both dispatch and dispatchAsync functions)

--- src/client/dispatcher.js
+++ src/client/dispatcher.js
@@ -4,32 +4,20 @@ import {$pendingActionsCursor} from './state';
 import {Dispatcher} from 'flux';
 
 const dispatcher = new Dispatcher;
-const isDev = 'production' !== process.env.NODE_ENV;
 
 export function register(callback: Function): string {
   return dispatcher.register(callback);
 }
 
-export function dispatch(action: Function, data: ?Object, options: ?Object) {
-  if (isDev && action.toString === Function.prototype.toString) {
-    throw new Error(`Action ${action} toString method has to be overridden by setToString.`);
-  }
-
-  const isAsync = data && typeof data.then === 'function';
-  if (isAsync) {
-    return dispatchAsync(action, data, options);
-  } else {
-    dispatchSync(action, data);
-  }
+export function dispatch(action: Function, data: ?Object) {
+  dispatcher.dispatch({action, data});
 }
 
-export function isPending(actionName) {
-  return $pendingActionsCursor().has(actionName);
-}
-
-function dispatchAsync(action: Function, promise: Object, options: ?Object) {
+export function dispatchAsync(action: Function, promise: object, options: ?Object) {
+  if (typeof promise === 'undefined') {
+    return null;
+  }
   const actionName = action.toString();
-  // Pending property is defined lazily.
   if (!action.hasOwnProperty('pending')) {
     Object.defineProperty(action, 'pending', {
       get: () => isPending(actionName)
@@ -40,7 +28,7 @@ function dispatchAsync(action: Function, promise: Object, options: ?Object) {
   return promise.then(
     (data) => {
       setPending(actionName, false);
-      dispatchSync(action, data);
+      dispatch(action, data);
       return data;
     },
     (reason) => {
@@ -50,13 +38,13 @@ function dispatchAsync(action: Function, promise: Object, options: ?Object) {
   );
 }
 
+export function isPending(actionName) {
+  return $pendingActionsCursor().has(actionName);
+}
+
 function setPending(actionName: string, pending: boolean) {
   $pendingActionsCursor($pendingActions => pending
     ? $pendingActions.set(actionName, true)
     : $pendingActions.delete(actionName)
   );
-}
-
-function dispatchSync(action: Function, data: ?Object) {
-  dispatcher.dispatch({action, data});
 }
