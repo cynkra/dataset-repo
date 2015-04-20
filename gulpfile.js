d1dd62b0245606467c983380c3fb5b692dac5962
Václav Ostrožlík <vaclav@ostrozlik.cz>
2015-04-20 11:28:42 +0200

    Removed jest (needs Python < 3.x.x)

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var bg = require('gulp-bg');
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
 var harmonize = require('harmonize');
-var jest = require('jest-cli');
 var makeWebpackConfig = require('./webpack/makeconfig');
 var runSequence = require('run-sequence');
 var webpackBuild = require('./webpack/build');
@@ -38,22 +37,6 @@ gulp.task('eslint', function() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
-});
-
-gulp.task('jest', function(done) {
-  var rootDir = './src';
-  jest.runCLI({config: {
-    'rootDir': rootDir,
-    'scriptPreprocessor': '../node_modules/babel-jest',
-    'testFileExtensions': ['es6', 'js'],
-    'moduleFileExtensions': ['js', 'json', 'es6']
-  }}, rootDir, function(success) {
-    /* eslint no-process-exit:0 */
-    done(success ? null : 'jest failed');
-    process.on('exit', function() {
-      process.exit(success ? 0 : 1);
-    });
-  });
 });
 
 gulp.task('test', function(done) {
