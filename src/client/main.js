1b615a9ef9af2ad47ef2a03bd6401a9a9939a5f3
Václav Ostrožlík <ostrovac@fit.cvut.cz>
2015-08-10 01:30:10 +0200

    Removed react-a11y

--- src/client/main.js
+++ src/client/main.js
@@ -4,10 +4,6 @@ import trackPageView from './common/trackPageView';
 
 const app = document.getElementById('app');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('react-a11y')();
-}
-
 router.run((Handler, state) => {
   React.render(<Handler />, app);
   trackPageView(state);
