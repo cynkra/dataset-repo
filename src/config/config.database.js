32b53c13390e65245046785134371eac0f5b6962
Václav Ostrožlík <ostrovac@fit.cvut.cz>
2015-08-10 10:50:18 +0200

    Fixed database login.

--- src/config/config.database.js
+++ src/config/config.database.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//   client:   'mysql',
-//   host:     'relational.fit.cvut.cz',
-//   user:     'guest',
-//   password: '******',
-//   database: 'meta'
-// };
-
 module.exports = {
   client:   'mysql',
-  host:     'localhost',
-  user:     'root',
-  password: '',
+  host:     'relational.fit.cvut.cz',
+  user:     'guest',
+  password: '******',
   database: 'meta'
 };
+
+// module.exports = {
+//   client:   'mysql',
+//   host:     'localhost',
+//   user:     'root',
+//   password: '',
+//   database: 'meta'
+// };
