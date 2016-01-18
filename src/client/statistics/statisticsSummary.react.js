9e790b16f846c94494bd3b84add4287ebdb0678e
Václav Ostrožlík <ostrovac@fit.cvut.cz>
2016-01-18 11:25:51 +0100

    Added Summary Statistics sort

--- src/client/statistics/statisticsSummary.react.js
+++ src/client/statistics/statisticsSummary.react.js
@@ -12,47 +12,91 @@ export default class StatisticsSummary extends Component {
     summary: React.PropTypes.instanceOf(immutable.List).isRequired
   }
 
+  constructor() {
+    super();
+    this.state = {
+      sortBy: 'title',
+      order: 'asc',
+      summary: immutable.List()
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      summary: nextProps.summary
+    });
+  }
+
+  sortBy(column) {
+    this.setState({
+      sortBy: column,
+      order: (column === this.state.sortBy && this.state.order === 'asc' ? 'desc' : 'asc')
+    });
+  }
+
+  renderHeaderCol(column, title) {
+    let className = '';
+    if (this.state.sortBy === column)
+      className += 'TableCol-active-' + this.state.order;
+    return (
+      <th
+        className={className}
+        onClick={() => this.sortBy(column)}
+      >{getNameWithTooltip(title)}</th>
+    );
+  }
+
   render() {
-    const summary = this.props.summary;
+    const summary = this.state.summary;
 
     return (
-      <table className='statisticsSummary'>
+      <table className='StatisticsSummary'>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>{getNameWithTooltip('#Relations')}</th>
-            <th>{getNameWithTooltip('#Attributes')}</th>
-            <th>{getNameWithTooltip('#Tuples')}</th>
-            <th>{getNameWithTooltip('#Instances')}</th>
-            <th>{getNameWithTooltip('Size')}</th>
-            <th>{getNameWithTooltip('Type')}</th>
-            <th>{getNameWithTooltip('Domain')}</th>
-            <th>{getNameWithTooltip('Task')}</th>
-            <th>{getNameWithTooltip('Missing values')}</th>
-            <th>{getNameWithTooltip('Loops')}</th>
-            <th>{getNameWithTooltip('Compound keys')}</th>
+            {this.renderHeaderCol('title', 'Name')}
+            {this.renderHeaderCol('tableCount', '#Relations')}
+            {this.renderHeaderCol('columnCount', '#Attributes')}
+            {this.renderHeaderCol('rowCount', '#Tuples')}
+            {this.renderHeaderCol('instanceCount', '#Instances')}
+            {this.renderHeaderCol('databaseSize', 'Size')}
+            {this.renderHeaderCol('isArtificial', 'Type')}
+            {this.renderHeaderCol('domain', 'Domain')}
+            {this.renderHeaderCol('task', 'Task')}
+            {this.renderHeaderCol('missingValues', 'Missing values')}
+            {this.renderHeaderCol('loops', 'Loops')}
+            {this.renderHeaderCol('compositeKeys', 'Compound keys')}
           </tr>
         </thead>
 
         <tbody>
-          {summary.map(s => {
-            return (
-              <tr key={s.title}>
-                <td><Link params={{title: s.title}} to='dataset'>{s.title}</Link></td>
-                <td>{s.tableCount}</td>
-                <td>{s.columnCount}</td>
-                <td>{s.rowCount}</td>
-                <td>{s.instanceCount}</td>
-                <td>{getSizeWithUnit(s.databaseSize)}</td>
-                <td>{s.isArtificial ? 'Synthetic' : 'Real'}</td>
-                <td>{s.domain}</td>
-                <td><abbr title={capitalize(s.task)}>{s.task.substr(0, 2).toUpperCase()}</abbr></td>
-                <td>{s.missingValues ? 'Yes' : 'No'}</td>
-                <td>{s.loops ? 'Yes' : 'No'}</td>
-                <td>{s.compoundKeys ? 'Yes' : 'No'}</td>
-              </tr>
-            );
-          })}
+          {summary
+            .sort((lineA, lineB) => {
+              const valA = lineA.get(this.state.sortBy);
+              const valB = lineB.get(this.state.sortBy);
+              if (valA < valB)
+                return this.state.order === 'asc' ? -1 : 1;
+              else if (valA > valB)
+                return this.state.order === 'asc' ? 1 : -1;
+              return 0;
+            })
+            .map(s => {
+              return (
+                <tr key={s.title}>
+                  <td><Link params={{title: s.title}} to='dataset'>{s.title}</Link></td>
+                  <td>{s.tableCount}</td>
+                  <td>{s.columnCount}</td>
+                  <td>{s.rowCount}</td>
+                  <td>{s.instanceCount}</td>
+                  <td>{getSizeWithUnit(s.databaseSize)}</td>
+                  <td>{s.isArtificial ? 'Synthetic' : 'Real'}</td>
+                  <td>{s.domain}</td>
+                  <td><abbr title={capitalize(s.task)}>{s.task.substr(0, 2).toUpperCase()}</abbr></td>
+                  <td>{s.missingValues ? 'Yes' : 'No'}</td>
+                  <td>{s.loops ? 'Yes' : 'No'}</td>
+                  <td>{s.compositeKeys ? 'Yes' : 'No'}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     );
