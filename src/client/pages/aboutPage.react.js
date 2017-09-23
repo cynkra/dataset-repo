f635e8be480fe6846796eeec343805e691231822
Jan Motl <jan@motl.us>
2017-09-23 14:04:01 +0200

    Improve help text

--- src/client/pages/aboutPage.react.js
+++ src/client/pages/aboutPage.react.js
@@ -27,15 +27,26 @@ export default class AboutPage extends Component {
           <section className='About-section'>
             <h2>FAQ</h2>
             <dl className='About-faq'>
-              <dt>Why the datasets are not stored in CSV files?</dt>
+              <dt>Why are the datasets not stored in CSV files?</dt>
               <dd>
-                Because CSV files don't store information about data types, PK, FK, indexes and constrains.
+                Because CSV files do not store information about data types, PKs, FKs and other constraints.
               </dd>
 
               <dt>Why MySQL database?</dt>
               <dd>
                 Because in combination with <a href="http://clowdflows.org">ClowdFlows</a> you can process the datasets online. <br />
                 Just open one of the public workflows (like <a href="http://clowdflows.org/workflows/copy-workflow/2222">Wordification</a> or <a href="http://clowdflows.com/workflow/4018">Cross-validation</a>), change the credentials in "MySQL Connect" operator to the credentials from the repository and you are ready to go!
+              </dd>
+
+              <dt>Why I am not able to connect to the database?</dt>
+              <dd>
+                If you are connecting to the database over a corporate network, the corporate firewalls could be the culprit. <br />
+                Try to access the database with a different internet provider (e.g. with your cellular provider). If the problems persist, contact us.
+              </dd>
+
+              <dt>Why MySQL Workbench complaints about incompatible/nonstandard server version?</dt>
+              <dd>
+                We are using open source version of MySQL called MariaDB, hence the warning. For all purposes that the public account permits it is safe to ignore the message.
               </dd>
 
               <dt>What to do if I want an ILP format?</dt>
@@ -51,7 +62,7 @@ export default class AboutPage extends Component {
               <dt>What's the point of including artificial datasets?</dt>
               <dd>
                 While datasets like Adventure Works may not contain any pattern that could be found during modeling, they still increase the diversity of the repository. For example, the named Adventure Works dataset has the highest table count in the whole repository. <br />
-                If your algorithm can process all the tables present in Adventure Works, it may be able to process real world datasets.
+                If your algorithm can process all the tables present in Adventure Works, it may be able to process real-world datasets.
               </dd>
             </dl>
           </section>
