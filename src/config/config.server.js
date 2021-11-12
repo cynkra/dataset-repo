b2329c2c41e05b2192e00fb184a53eebcfbc4425
Jan Motl <jan@motl.us>
2021-11-12 16:11:28 +0100

    Update e-mail settings

--- src/config/config.server.js
+++ src/config/config.server.js
@@ -31,13 +31,13 @@ var config = {
   },
   email: {
     transporter: {
-      service: 'Gmail',  // https://nodemailer.com/smtp/well-known/
+      service: 'Gmail',  // sets all the parameters: https://nodemailer.com/smtp/well-known/
       auth: {
-        user: "watchdog.jm@gmail.com",  // This is a throwaway account -> it is OK to use hardcoded credentials
+        user: "watchdog.jm@gmail.com",  // this is a throwaway account -> it is OK to use hardcoded credentials
         pass: "StrongHrad99"
       }
     },
-    recipient: 'Jan Motl <jan.motl@fit.cvut.cz>' // , Oliver Schulte <oschulte@cs.sfu.ca>
+    recipient: 'Jan Motl <jan.motl@fit.cvut.cz>, Oliver Schulte <oschulte@cs.sfu.ca>'
   },
   googleAnalyticsId: 'UA-61229872-1',
   isProduction: process.env.NODE_ENV === 'production',
